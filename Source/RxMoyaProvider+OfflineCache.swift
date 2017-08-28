10264a3dff8e5697f7b47b8d407003eb287f35ae
魏武涛 <mac@jingtaoMac.local>
2017-08-28 15:40:51 +0800

    fix: offlineCache策略更改

--- Source/RxMoyaProvider+OfflineCache.swift
+++ Source/RxMoyaProvider+OfflineCache.swift
@@ -11,41 +11,39 @@ import RxSwift
 import Moya
 import Result
 
+protocol CacheProtocol {
+    func readOffLineCache() -> Any?
+}
+
 extension RxMoyaProvider {
-    func tryUseOfflineCacheThenRequest(_ token: Target, _ cacheType: CacheType) -> Observable<Moya.Response> {
+    func tryUseOfflineCacheThenRequest(_ token: Target, _ cacheProtocol: CacheProtocol) -> Observable<Moya.Response> {
         return Observable.create { [weak self] observer -> Disposable in
-            
-            let key = token.path
             
             var cancelableToken: Cancellable?
             
             // 先读取缓存内容，有则发出一个信号（onNext），没有则跳过
-            // 后期缓存可以区分文件、数据库、内存
-            CacheManager.sharedInstance.readCacheAsync(key, cacheType, { (object) in
+            if cacheProtocol.readOffLineCache() != nil {
                 
-                if object != nil {
+                do {
+                    let prettyData =  try JSONSerialization.data(withJSONObject: cacheProtocol.readOffLineCache()!, options: .prettyPrinted)
+                    observer.onNext(Response(statusCode: 0, data: prettyData))
                     
-                    do {
-                        let prettyData =  try JSONSerialization.data(withJSONObject: object!, options: .prettyPrinted)
-                        observer.onNext(Response(statusCode: 200, data: prettyData))
-                        
-                    } catch {
-                        
-                    }
+                } catch {
                     
                 }
                 
-                cancelableToken = self?.request(token) { result in
-                    
-                    switch result {
-                    case let .success(response):
-                        observer.onNext(response)
-                        observer.onCompleted()
-                    case let .failure(error):
-                        observer.onError(error)
-                    }
+            }
+            
+            cancelableToken = self?.request(token) { result in
+                
+                switch result {
+                case let .success(response):
+                    observer.onNext(response)
+                    observer.onCompleted()
+                case let .failure(error):
+                    observer.onError(error)
                 }
-            })
+            }
             
             return Disposables.create {
                 cancelableToken?.cancel()
